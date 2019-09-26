8debe80791a0b3ee1139bfbbd1632c2815994c04
t198769 <ilmari2012@gmail.com>
2019-09-26 13:52:30 +0300

    IfTask 1 Ready

--- IfTask 1/IfTask 1/Program.cs
+++ IfTask 1/IfTask 1/Program.cs
@@ -6,7 +6,26 @@ namespace IfTask_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvitää käyttäjän syöttämästä luvusta, onko se positiivinen vai negatiivinen!");
+            Console.Write("Syötä kokonaisluku: ");
+            int userInputNumber = int.Parse(Console.ReadLine());
+
+            if( userInputNumber == 0)
+            {
+                Console.WriteLine($"Luku {userInputNumber} on nolla!");
+            }
+            else
+            {
+                if(userInputNumber > 0)
+                {
+                    Console.WriteLine($"Luku {userInputNumber} on positiivinen");
+                }
+                else
+                {
+                    Console.WriteLine($"Luku {userInputNumber} on negatiivinen");
+                }
+            }
+
         }
     }
 }
