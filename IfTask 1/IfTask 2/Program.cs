b5bd8661cc4d10d4bc09745f7692c55617efa543
t198769 <ilmari2012@gmail.com>
2019-10-01 11:12:25 +0300

    IfTask 1&2&3 Completed

--- IfTask 1/IfTask 2/Program.cs
+++ IfTask 1/IfTask 2/Program.cs
@@ -6,7 +6,19 @@ namespace IfTask_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjemla selvittää onko luku pariton vai parillinen!");
+            int userInputNumber = 0;
+            Console.WriteLine("Syötä kokonaisluku: ");
+            userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber % 2 == 0)
+            {
+                Console.WriteLine($"Luku {userInputNumber} on parillinen");
+            }
+
+            else
+            {
+                Console.WriteLine($"Luku {userInputNumber} on pariton");
+            }
         }
     }
 }
---
+++ IfTask 1/IfTask 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IfTask_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInputNumber = 0;
+            Console.WriteLine("Ohjelma selvittää onko numero pariton tai parillinen ja positiivinen tai negatiivinen!");
+            Console.WriteLine("Syötä luku: ");
+            userInputNumber = int.Parse(Console.ReadLine());
+
+            if (userInputNumber < 0)
+
+                Console.Write($"Numero {userInputNumber} on negatiivinen");
+
+            else
+                Console.Write($"Numero {userInputNumber} on positiivnen");
+            {
+                if (userInputNumber % 2 == 0)
+                    Console.WriteLine(" ja parillinen");
+                else
+                    Console.WriteLine(" ja pariton");
+
+            }
+
+        }
+    }
+}
