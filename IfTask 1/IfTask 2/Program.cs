7c21fbcdf6fa525f65f524da6a2cbd6f264e87c8
t198769 <ilmari2012@gmail.com>
2019-10-01 08:58:54 +0300

    IfTask 2 Created

---
+++ IfTask 1/IfTask 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IfTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjemla selvittää onko luku pariton vai parillinen!");
+        }
+    }
+}
