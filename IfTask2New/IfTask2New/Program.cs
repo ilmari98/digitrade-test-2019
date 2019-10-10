d640bb048ef71911996e52da90905215331f3e4c
t198769 <ilmari2012@gmail.com>
2019-10-10 11:32:53 +0300

    IfTask2 Completed

---
+++ IfTask2New/IfTask2New/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace IfTask2New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kuinka paljon maksa lippu");
+            Console.WriteLine("Syötä ikäsi");
+            int userInputAge = int.Parse(Console.ReadLine());
+            int normalTicket = 16;
+            double discount = 0;
+            double servicemanDiscount, childDiscount, retiredDiscount = 0.5;
+            double mtkMemberDiscount = 0.15;
+            double studentDiscount = 0.45;
+            string isConscript, isStudent, isMtkMember = "";
+            if (userInputAge <= 7)
+            {
+                Console.WriteLine($"Hinta on {normalTicket * discount} Euroa ");
+            }
+            else if (userInputAge > 7 && userInputAge < 15)
+            {
+                Console.WriteLine($"Hinta on {normalTicket * childDiscount} Euroa ");
+            }
+            else if (userInputAge >= 15 && userInputAge < 65)
+            {
+                Console.WriteLine($"Oletko varusmis? y/n");
+                isConscript = Console.ReadLine();
+                if (isConscript == "y")
+                {
+                    Console.WriteLine($"Hinta on {normalTicket - (normalTicket * servicemanDiscount)} Euroa ");
+                }
+                else
+                {
+                    Console.WriteLine($"Oletko opiskelia? y/n");
+                    isStudent = Console.ReadLine();
+                    if (isStudent == "y")
+                    {
+                        Console.WriteLine($"Oletko Mtk jäsen? y/n");
+                        isMtkMember = Console.ReadLine();
+                        if (isMtkMember == "y")
+                        {
+                            Console.Write($"Hinta on {normalTicket - (normalTicket * (studentDiscount + mtkMemberDiscount))} Euroa ");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Hinta on {normalTicket - (normalTicket * studentDiscount)} Euroa");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Oletko Mtk jäsen? y/n");
+                        isMtkMember = Console.ReadLine();
+                        if (isMtkMember == "y")
+                        {
+                            Console.Write($"Hinta on {normalTicket - (normalTicket * mtkMemberDiscount)} Euroa ");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Hinta on {normalTicket} Euroa");
+                        }
+                    }
+                }
+            }
+            else if (userInputAge >= 65)
+            {
+                Console.WriteLine($"Hinta on {normalTicket - (normalTicket * retiredDiscount)} Euroa ");
+            }
+        }
+    }
+}
