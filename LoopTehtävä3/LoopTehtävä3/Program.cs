c65925ff1b9aae79c5e249ce24cee5d04c8cf8d3
t198769 <Sharpgrapevines1>
2019-10-30 10:32:12 +0200

    LoopTask3 Created

---
+++ LoopTehtävä3/LoopTehtävä3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoopTehtävä3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee ohjelma, joka laskee N:n ensimmäisen parittoman ja parillisen lukujen summan");
+        }
+    }
+}
