9fc9e126267e598cfec421cb5bb1f08ea1692087
t198769 <Sharpgrapevines1>
2019-10-30 11:29:16 +0200

    LoopTask4 Completed

---
+++ LoopTehtävä4/LoopTehtävä4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTehtävä4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee n ensimmäisein lukua yhteen!");
+            Console.Write("Syötä luku:");
+            int userInput = int.Parse(Console.ReadLine());
+            int sumOfNumbers = 0;
+
+            if (userInput > 0)
+            {
+                Console.WriteLine("Virheellinen numero");
+            }
+            else
+            {
+                for (int i = 0; userInput < 0; userInput++)
+                {
+                    sumOfNumbers = sumOfNumbers + userInput;
+                }
+
+                Console.WriteLine($"Vastaus: {sumOfNumbers}");
+            }
+        }
+    }
+}
