23c9ff5852b68dadc047d16619b08572b4e9d6e4
t198769 <Sharpgrapevines1>
2019-10-30 10:56:24 +0200

    LoopTask3 Completed

--- LoopTehtävä3/LoopTehtävä3/Program.cs
+++ LoopTehtävä3/LoopTehtävä3/Program.cs
@@ -7,6 +7,24 @@ namespace LoopTehtävä3
         static void Main(string[] args)
         {
             Console.WriteLine("Tee ohjelma, joka laskee N:n ensimmäisen parittoman ja parillisen lukujen summan");
+            Console.Write("Syötä luku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for(int i = 0;i < userInput; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i;
+                }
+                else
+                {
+                    oddSum = oddSum + i;
+                }
+            }
+            Console.WriteLine($"Parillisten summa = {evenSum}\n" +
+                $"parittomien summa = {oddSum}");
         }
     }
 }
