207ca4716ba07c1437e0f49b730df528c1674284
t198769 <Sharpgrapevines1>
2019-11-06 10:12:59 +0200

    LoopTehtävä5 Valmis

---
+++ LoopTehtävä5/LoopTehtävä5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTehtävä5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen lukujen summan.");
+            Console.Write("Syötä luku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            int parillinen = 0;
+            int pariton = 0;
+
+            {
+                for (int i = -1; i >= userInput; i--)
+                {
+                    if (i % 2 == 0)
+                        parillinen = parillinen + i;
+                    else
+                        pariton = pariton + i;
+                }
+            }
+
+            Console.WriteLine($"Parillisten summa = {parillinen}\n" +
+                $"Parittomien summa = {pariton}");
+        }
+    }
+}
