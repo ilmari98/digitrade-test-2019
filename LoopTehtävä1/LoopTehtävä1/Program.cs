848096dd37dad6f2dabadbd6adf21573fcd3a9db
t198769 <Sharpgrapevines1>
2019-11-06 10:11:02 +0200

    LoopTehtävä1 Valmis

---
+++ LoopTehtävä1/LoopTehtävä1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoopTehtävä1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee kertoman luvun itsellään kasvaen n lukuun asti!");
+            Console.WriteLine("syötä luku!");
+            int userInput = int.Parse(Console.ReadLine());
+            int fact = 1;
+            int n = userInput;
+
+
+            if (n < 0)
+            {
+                Console.WriteLine("Virheellinen syöte!");
+            }
+            else
+            {
+                while (n > 0)
+                {
+                    fact = fact * n;
+                    n--;
+                }
+            }
+            Console.WriteLine("Vastaus: " + fact);
+        }
+    }
+}
