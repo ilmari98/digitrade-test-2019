d32674daeae05e30c85217c6c508fbbbc1f5b286
t198769 <Sharpgrapevines1>
2019-11-06 10:11:33 +0200

    LoopTehtävä2 Valmis

---
+++ LoopTehtävä2/LoopTehtävä2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTehtävä2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee n ensimmäisein lukua yhteen!");
+            Console.Write("Syötä luku:");
+            int userInput = int.Parse(Console.ReadLine());
+            int sumOfNumbers = 0;
+
+            if (userInput < 0)
+            {
+                Console.WriteLine("Virheellinen numero");
+            }
+            else
+            {
+                for (int i = 0; i < userInput; i++)
+                {
+                    sumOfNumbers = sumOfNumbers + (i + 1);
+                }
+
+                Console.WriteLine($"Vastaus: {sumOfNumbers}");
+            }
+        }
+    }
+}
