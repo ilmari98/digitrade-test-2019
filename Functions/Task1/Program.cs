1133384723f35fdb951c9eb160972deb0d5a89f0
t198769 <Sharpgrapevines1>
2019-11-12 10:15:18 +0200

    Function Tasks 1 & 2 Done

---
+++ Functions/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma tulostaa syötetyn numeron mukaisen määrän tähtiä!");
+            Console.Write("Syötä kokonaisluku: ");
+            int userInput = int.Parse(Console.ReadLine());
+            if (IsValidNumber(userInput))
+            {
+                string strStars = GenerateAmountOfStars(userInput);
+                Console.WriteLine(strStars);
+            }
+            else
+            {
+                Console.WriteLine($"Numero {userInput} ei ole sallittu luku!");
+            }
+
+        }
+
+        static bool IsValidNumber(int userInput)
+        {
+            if (userInput > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string GenerateAmountOfStars(int starCount)
+        {
+            string stars = "";
+            for (int i = 0; i < starCount; i++)
+            {
+                stars = stars + "*";
+            }
+            return stars;
+        }
+    }
+}
---
+++ Functions/Task2/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma!");
+            Console.Write("Syötä 1.luku : ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Syötä 2.Luku : ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Luku {GetMinNumber(x, y)} on pienempi kuin {GetMaxNumber(x, y)}");
+        }
+
+
+
+        static int GetMinNumber(int firstnumber, int lastnumber)
+        {
+            if(firstnumber < lastnumber)
+                {
+                return firstnumber;
+                }
+                else
+                {
+                return lastnumber;
+                }
+        }
+        static int GetMaxNumber(int lastNumber,int firstNumber)
+        {
+            if (firstNumber < lastNumber)
+            {
+                return lastNumber;
+            }
+            else
+            {
+                return firstNumber;
+            }
+        }
+    }
+}
