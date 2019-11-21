56e61d089ad6e1c328957c73f5c40f9f4e084d70
t198769 <Sharpgrapevines1>
2019-11-21 10:29:48 +0200

    Task3 Done, now disqualifies text

--- Functions/Task3New/Task3New/Program.cs
+++ Functions/Task3New/Task3New/Program.cs
@@ -6,31 +6,42 @@ namespace Task3New
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sovellus kysyy kaksi lukua ja antaa niiden välistä numeron!");
+            Console.WriteLine("Sovellus kysyy kaksi lukua ja laskee käykö kolmas syöttö käy niiden väliin!");
             Console.Write("Syötä ensimmäinen numero: ");
             int numberOne = int.Parse(Console.ReadLine());
             Console.Write("Syötä Toinen numero: ");
             int numberTwo = int.Parse(Console.ReadLine());
-            Console.WriteLine("Syötä Kolmas luku jonka ohjelma tarkistaa ensimmästen kahden luvun välistä");
-            int number = int.Parse(Console.ReadLine());
-            int temporayThirdNumber = 0;
-            if (numberOne > numberTwo)
-            {
-                temporayThirdNumber = numberTwo;
-                numberTwo = numberOne;
-                numberOne = temporayThirdNumber;
-            }
-                bool returnedValue = NumberFromRange(numberOne, number, numberTwo);
-            if (returnedValue == true)
-            {
-                Console.WriteLine($"{number} on {numberOne} ja {numberTwo} välissä!");
-            }
-            else
-            {
-                Console.WriteLine($"Syöttämä numero {number} ei ollut aikaisempien numeroiden välissä");
-            }
-        }
 
+            do
+            {
+                Console.WriteLine("Syötä Kolmas luku jonka ohjelma tarkistaa ensimmästen kahden luvun välistä: ");
+                bool isValidNumber = int.TryParse(Console.ReadLine(), out int number);
+                if (isValidNumber = true)
+                {
+
+                    int temporayThirdNumber = 0;
+                    if (numberOne > numberTwo)
+                    {
+                        temporayThirdNumber = numberTwo;
+                        numberTwo = numberOne;
+                        numberOne = temporayThirdNumber;
+                    }
+                    bool returnedValue = NumberFromRange(numberOne, number, numberTwo);
+                    if (returnedValue == true)
+                    {
+                        Console.WriteLine($"\nSyöttämäsi luku {number} on numeroiden {numberOne} ja {numberTwo} välissä!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Syöttösi ei käy aikaisempien numeroiden väliin");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Sovellukseen ei käy teksti, kirjoita jokin numero");
+                }
+            } while (true);
+        }
         static bool NumberFromRange(int lowerBound, int number, int upperBound)
         {
 
