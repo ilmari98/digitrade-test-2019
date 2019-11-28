a74c835f5c7798dc2bc98c3a4f849e6d676f58fa
t198769 <Sharpgrapevines1>
2019-11-28 08:43:41 +0200

    ArrayTask1 Started

---
+++ ArrayTasks/ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArrayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Taulokko harjoitus 1");
+
+        }
+    }
+}
