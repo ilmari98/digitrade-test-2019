8552135434197971f04e8ff33b3e151984d4111e
ilmari98 <Sharpgrapevines1>
2020-01-05 20:39:14 +0200

    ArrayTask2 Started

---
+++ ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma arpoo 50 lukua 0-100 välistä ja kertoo niiden keskiarvon");
+        }
+    }
+}
