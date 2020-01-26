36160786e7a3b365ed00cfb6544aca7bca2afbbd
ilmari98 <ilmari2012@gmail.com>
2020-01-26 14:58:24 +0200

    ArrayTask2 Done

--- ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
+++ ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
@@ -7,15 +7,23 @@ namespace ArrayTask2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma arpoo 100 lukua 0-50 välistä ja kertoo niiden keskiarvon");
-            Random rnd = new Random();
+            Random rnd = new Random(); //rnd.Next(50);
             double average = 0;
-            int numberRange = 100;
-            int[] iT = { numberRange };
 
-            for (int i = 0; i < numberRange; i++)
+            int[] iT = new int[100];
+
+            for (int i = 0; i < iT.Length; ++i)
             {
                 Console.WriteLine($"{i+1}. {rnd.Next(50)}");
             }
+
+            double total = 0.0;
+            for (int i = 0; i < iT.Length; ++i)
+            {
+                total += rnd.Next(50);
+            }
+            average = total / iT.Length;
+            Console.WriteLine($"keskiarvo on {average}");
         }
     }
 }
