5300e39728076f13dd317336ad69ad2258e307b9
ilmari98 <ilmari2012@gmail.com>
2020-01-26 13:58:34 +0200

    Program Successfully Generates 50 Random Numbers.

--- ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
+++ ArrayTasks/ArrayTask2/ArrayTask2/Program.cs
@@ -6,7 +6,16 @@ namespace ArrayTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma arpoo 50 lukua 0-100 välistä ja kertoo niiden keskiarvon");
+            Console.WriteLine("Ohjelma arpoo 100 lukua 0-50 välistä ja kertoo niiden keskiarvon");
+            Random rnd = new Random();
+            double average = 0;
+            int numberRange = 100;
+            int[] iT = { numberRange };
+
+            for (int i = 0; i < numberRange; i++)
+            {
+                Console.WriteLine($"{i+1}. {rnd.Next(50)}");
+            }
         }
     }
 }
