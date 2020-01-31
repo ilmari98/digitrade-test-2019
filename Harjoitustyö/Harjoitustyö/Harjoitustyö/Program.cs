d1b0896bc12638cdaab1966340100998923765a3
ilmari98 <ilmari2012@gmail.com>
2020-01-31 08:03:14 +0200

    Harjoitustyö Started

---
+++ Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Harjoitustyö
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä viitenumero ja sovellus selvittää onko se sopiva numerosarja.");
+        }
+    }
+}
