96cd3c0c9e6c295dfe8b2063378f798273a07195
ilmari98 <ilmari2012@gmail.com>
2020-01-31 08:16:07 +0200

    Program now rejects inputted reference numbers that are too long or too short

--- Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
+++ Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
@@ -6,7 +6,16 @@ namespace Harjoitustyö
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Syötä viitenumero ja sovellus selvittää onko se sopiva numerosarja.");
+            Console.WriteLine("Syötä viitenumero ja sovellus selvittää onko se sopiva numerosarja.\n");
+            string refernceNumber = Console.ReadLine();
+            if(refernceNumber.Length > 19 )
+            {
+                Console.WriteLine($"\nViitenumerosi on liian pitkä.");
+            }
+            if (refernceNumber.Length < 4)
+            {
+                Console.WriteLine($"\nViitenumerosi on liian lyhyt.");
+            }
         }
     }
 }
