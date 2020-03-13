4551b11ac84cc9d6ba5cba2ec4b5bed5eff0eef7
Ilmari98 <ilmari2012@gmail.com>
2020-03-13 12:23:30 +0200

    Program can now give correct last number for referencenumber -query

--- Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
+++ Harjoitustyö/Harjoitustyö/Harjoitustyö/Program.cs
@@ -7,14 +7,66 @@ namespace Harjoitustyö
         static void Main(string[] args)
         {
             Console.WriteLine("Syötä viitenumero ja sovellus selvittää onko se sopiva numerosarja.\n");
-            string refernceNumber = Console.ReadLine();
-            if(refernceNumber.Length > 19 )
+            int refTotal = 0;
+            int[] BBANvalseries = { 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1 };
+
+            char userChoice;
+            do
             {
-                Console.WriteLine($"\nViitenumerosi on liian pitkä.");
+                userChoice = UI();
+                switch (userChoice)
+                {
+                    case '1':
+                        Console.WriteLine("\n\nSyötä viitenumero ja sovellus selvittää sen viimeisen numeron");
+                        string refSansLast = Console.ReadLine();
+                        string newRefNumber = RemoveSpaces(refSansLast);
+                        SumOfRefNum(newRefNumber);
+                        Console.WriteLine($"Viimeinen numero viitesarjallesi olisi {RoundUp(refTotal) - refTotal}.");
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        return;
+                    default:
+                        Console.WriteLine($"\nError! Valintasi [{userChoice}] ei ole vaihtoehto!\n");
+                        break;
+                }
+            } while (((userChoice != '1') && (userChoice != '2') && (userChoice != '3')));
+
+    {
+
             }
-            if (refernceNumber.Length < 4)
+
+            string SumOfRefNum(string refSansLast)
             {
-                Console.WriteLine($"\nViitenumerosi on liian lyhyt.");
+                int Length = refSansLast.Length;
+                for (int i = 0; i < Length; i++)
+                {
+                    refTotal += (refSansLast[i] * BBANvalseries[i]) - BBANvalseries[i] * 48;
+                }
+                return $"Viimeinen numero on {refTotal}";
+            }
+
+            static string RemoveSpaces(string userInput)
+            {
+                string result = userInput.Replace(" ", "");
+                return result;
+            }
+
+            int RoundUp(int roundUp)
+            {
+                if (roundUp % 10 == 0) return roundUp;
+                return (10 - roundUp % 10) + roundUp;
+            }
+
+            static char UI()
+            {
+                Console.WriteLine("Viitenumero sovellus");
+                Console.WriteLine("[1] Hanki viitenumerollesi viimeinen numero.");
+                Console.WriteLine("[2] Testaa viitenumerosi aitouden.");
+                Console.WriteLine("[3] Sulje ohjelma.");
+                Console.Write("valitse mitä tehdään: \n\n");
+                return char.ToUpper(Console.ReadKey().KeyChar);
             }
         }
     }
